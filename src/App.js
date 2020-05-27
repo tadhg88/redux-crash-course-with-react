fd7c4b6c18435cc343b6df225df516f1e9eae62f
Tim Whelan <twhelan@dr.local>
2020-05-27 22:59:53 +0100

    adding form to add post

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 
 import Posts from "./components/Posts";
+import PostForm from "./components/Postform";
 
 function App() {
   return (
     <div className="App">
+      <PostForm />
       <Posts />
     </div>
   );
---
+++ src/components/Postform.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+
+class PostForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      body: "",
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label>Title:</label>
+            <br />
+            <input
+              type="text"
+              name="title"
+              onChange={this.onChange}
+              value={this.state.title}
+            />
+          </div>
+          <div>
+            <label>Body:</label>
+            <br />
+            <textarea
+              name="body"
+              onChange={this.onChange}
+              value={this.state.body}
+            />
+          </div>
+          <br />
+          <button type="submit">Submit</button>
+          <br />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default PostForm;
