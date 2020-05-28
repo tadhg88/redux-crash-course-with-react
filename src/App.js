8bef02716d587bc86c11886e4dbafc01f6d024fd
tadhg88 <tadhg88@gmail.com>
2020-05-28 09:25:02 +0100

    moving redux store to separate file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
 
 import Posts from "./components/Posts";
 import PostForm from "./components/Postform";
