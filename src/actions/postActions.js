51a96070f6fd5f48cd376c2f1c50164c8d2cf34f
Tim <tadhg88@gmail.com>
2020-05-28 09:59:47 +0100

    updating to use ES6 syntax

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,15 +1,13 @@
 import { FETCH_POSTS, NEW_POST } from "./types";
 
 // action creator??
-export function fetchPosts() {
-  return function (dispatch) {
-    fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
-      res.json().then((posts) =>
-        /* dispatch sstate to reducer*/ dispatch({
-          type: FETCH_POSTS,
-          payload: posts,
-        })
-      )
-    );
-  };
-}
+export const fetchPosts = () => (dispatch) => {
+  fetch("https://jsonplaceholder.typicode.com/posts").then((res) =>
+    res.json().then((posts) =>
+      /* dispatch state to reducer*/ dispatch({
+        type: FETCH_POSTS,
+        payload: posts,
+      })
+    )
+  );
+};
